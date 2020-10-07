197f049e1ccac789a8ca22d8bb316c0f84873572
umgelurgel <umgelurgel@gmail.com>
2020-10-07 19:07:57 +0200

    Fixing linting.

--- etl_function/test_utils.py
+++ etl_function/test_utils.py
@@ -2,7 +2,7 @@ import responses
 
 from utils import new_york_times_data_uri, john_hopkins_data_url, fetch_csv, get_data
 
-NYT_DATA_EXTRACT = '''date,cases,deaths
+NYT_DATA_EXTRACT = """date,cases,deaths
 2020-01-21,1,0
 2020-01-22,1,0
 2020-01-23,1,0
@@ -10,9 +10,9 @@ NYT_DATA_EXTRACT = '''date,cases,deaths
 2020-01-25,3,0
 2020-01-26,5,0
 2020-01-27,5,0
-2020-01-28,5,0'''
+2020-01-28,5,0"""
 
-JH_DATA_EXTRACT = '''Date,Country/Region,Province/State,Lat,Long,Confirmed,Recovered,Deaths
+JH_DATA_EXTRACT = """Date,Country/Region,Province/State,Lat,Long,Confirmed,Recovered,Deaths
 2020-10-04,Turkey,,38.9637,35.2433,324443,285050,8441
 2020-10-05,Turkey,,38.9637,35.2433,326046,286370,8498
 2020-10-06,Turkey,,38.9637,35.2433,327557,287599,8553
@@ -22,7 +22,7 @@ JH_DATA_EXTRACT = '''Date,Country/Region,Province/State,Lat,Long,Confirmed,Recov
 2020-01-25,US,,40.0,-100.0,2,0,0
 2020-01-26,US,,40.0,-100.0,5,0,0
 2020-01-27,US,,40.0,-100.0,5,0,0
-2020-01-28,US,,40.0,-100.0,5,0,0'''
+2020-01-28,US,,40.0,-100.0,5,0,0"""
 
 
 @responses.activate
@@ -31,8 +31,8 @@ def test_fetch_csv():
 
     csv_lines = fetch_csv(new_york_times_data_uri)
 
-    assert '\n'.join(csv_lines).islower()
-    assert '\n'.join(csv_lines) == NYT_DATA_EXTRACT
+    assert "\n".join(csv_lines).islower()
+    assert "\n".join(csv_lines) == NYT_DATA_EXTRACT
 
 
 @responses.activate
@@ -43,10 +43,10 @@ def test_get_data():
     data = get_data()
 
     # Check the off by one data point handling works as expected
-    assert len(data) == len(NYT_DATA_EXTRACT.split('\n')) - 1
+    assert len(data) == len(NYT_DATA_EXTRACT.split("\n")) - 1
 
     # Check all keys exist in the record
-    assert 'date' in data[0]
-    assert 'cases' in data[0]
-    assert 'deaths' in data[0]
-    assert 'recoveries' in data[0]
+    assert "date" in data[0]
+    assert "cases" in data[0]
+    assert "deaths" in data[0]
+    assert "recoveries" in data[0]
